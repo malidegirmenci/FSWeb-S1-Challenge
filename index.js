2511fa6c7d546e5f552b80dc5a83c4699c21debc
Mehmet Ali <86076087+malidegirmenci@users.noreply.github.com>
2023-08-25 15:32:13 +0300

    Sprint 1 Challange completed

--- index.js
+++ index.js
@@ -149,15 +149,17 @@ Aşağıdakileri konsolda gösterim (console.log) işlemi gerçekleştirerek, yu
 (işlev yazmanıza gerek yok) */
 
 //(1) Dizideki ilk fenomen (0. dizin) profil (profile) adı
-
+console.log(fenomenler[0].profile);
 
 //(2) Dizideki üçüncü fenomenin (2. dizin) takipçi (followers) sayısı
-
+console.log(fenomenler[2].followers);
 
 /* Görev 2 (otomatik kontrol testi yapılmayacak):
 (işlev yazmanıza gerek yok)
 Fenomenler dizisinde bir yazım hatası var 😱 7. sıradaki fenomen 'Justin Bieber' ın soyismi 'Biber' olarak yanlış yazılmış. Bu sorunu düzeltin ve çalışmanızı kontrol etmek için console.log() yapın.
-
+*/
+fenomenler[6].profile = "Justin Bieber";
+console.log(fenomenler[6].profile);
 
 /*  Görev 3:
 Aşağıdaki işlemleri yapmak için indekseGoreFenomen işlevini kullanın:
@@ -168,8 +170,8 @@ Aşağıdaki işlemleri yapmak için indekseGoreFenomen işlevini kullanın:
 NOT: DÖNDÜĞÜNÜZ DİZİN YUKARIDAKİ BİÇİMLE EŞLEŞMESİ GEREKİR, YA DA TESTİ GEÇMEYECEKTİR!
 ÖRNEK: fenomenler dizisi ve 3 sayısı ile indekseGoreFenomen çağrılırsa, `3. indekste bulunan fenomen: Leo Messi' */
 
-function indekseGoreFenomen(/*kod*/) {
-  /*kod*/
+function indekseGoreFenomen(arr,index) {
+  return `${index}. indekste bulunan fenomen: ${arr[index].profile}`;
 }
 
 
@@ -182,8 +184,12 @@ Aşağıdakileri yapmak için profilListesi'ni kullanın:
 🌟 Dönüş ÖRNEĞİ: ["Instagram", "Cristiano Ronaldo", "Kylie"....]
 */
 
-function profilListesi(/*kod*/) {
-  /*kod*/
+function profilListesi(arr) {
+  const newArr = [];
+  for(let i = 0; i < arr.length; i++){
+    newArr[i] = arr[i].profile;
+  }
+  return newArr
 }
 
 
@@ -197,8 +203,11 @@ Aşağıdakileri yapmak için fenomenSil'i kullanın:
 5. Ortaya çıkan diziyi döndürün
 
 ÖRNEK: fenomenSil işlevi fenomenler dizisi ve 0 indeks sayısı ile çağrılırsa, veri kümemizden 'Instagram' kaldırılmış olarak döndürür. */
-function fenomenSil(/*kod*/) {
-  /*kod*/
+function fenomenSil(arr,index) {
+  const newArr = [...arr];
+  newArr.splice(index,1);
+  return newArr
+
 }
 
 
@@ -220,8 +229,17 @@ Aşağıdakileri yapmak için fenomenEkle'i kullanın:
 
 ÖRNEK: fenomenEkle(fenomenler, 6, "Workintech", 10000000, 2022, "Instagram") çağrıldığında dizinin sonuna yukarıdaki nesne en sona eklenerek yeni fenomenler dizisini döndürmelidir. */
 
-function fenomenEkle(/*kod*/) {
-  /*kod*/
+function fenomenEkle(arr,number,profile,followers,posts,platform) {
+  const newArr = [...arr];
+  let newFenomen = {
+    number : number,
+    profile : profile,
+    followers : followers,
+    posts : posts,
+    platform : platform
+  };
+  newArr.push(newFenomen);
+  return newArr;
 }
 
 
@@ -233,8 +251,14 @@ Aşağıdakileri yapmak için enFenomenler'yi kullanın:
 ÖRNEK: enFenomenler(fenomenler) çağrıldığında sonuç olarak ["Instagram", "Cristiano Ronaldo", ... "Khabane lame"] dönemelidir
 */
 
-function enFenomenler(/*kod*/) {
-  /*kod*/
+function enFenomenler(arr) {
+  const newArr = [];
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i].followers >= 100000000){
+      newArr.push(arr[i].profile);
+    }
+  }
+  return newArr
 }
 
 
@@ -247,8 +271,12 @@ Aşağıdakileri yapmak için fenomenGonderimSayisi'nı kullanın:
 ÖRNEK: fenomenGonderimSayisi(fenomenler, 'Will Smith') çağrıldığında "136" dönmelidir
 */
 
-function fenomenGonderimSayisi(/*kod*/){
-  /*kod*/
+function fenomenGonderimSayisi(arr,nameFenomen){
+  for(let i = 0; i < arr.length;i++){
+    if(arr[i].profile === nameFenomen){
+      return arr[i].posts
+    }
+  }
 }
 
 
@@ -264,8 +292,24 @@ Not: Gönderi sayısı belli olmayan (NA) hesaba katmayın.
 Örnek: platformaGoreCokGonderiYapanFenomen(fenomenler, 'TikTok') çağrıldığında "charli damelio" dönmelidir
 */
 
-function platformaGoreCokGonderiYapanFenomen(/*kod*/){
-  /*kod*/
+function platformaGoreCokGonderiYapanFenomen(arr,platform){
+  const samePlatformArr = [];
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i].platform === platform){
+      if(arr[i].posts !== "NA"){
+        samePlatformArr.push(arr[i])
+      }
+    }
+  }
+  let max = 0;
+  let hasMaxPostProfile = "";
+  for(let i = 0; i < samePlatformArr.length;i++){
+      if(max < samePlatformArr[i].posts){
+        max = samePlatformArr[i].posts
+        hasMaxPostProfile = samePlatformArr[i].profile
+      }
+  }
+  return hasMaxPostProfile
 }
 
 
